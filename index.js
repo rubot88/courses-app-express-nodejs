59169bb32e94ff3f7276a83ebfde5a684e59f3ee
Rustam Botirov <rustambotirov88@gmail.com>
2020-01-05 10:06:24 +0200

    add navigation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 
 const app = express();
@@ -11,6 +12,8 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index');
