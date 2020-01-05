ebbf5bb08580aa55dfcd08b376551f3a4c81b52f
Rustam Botirov <rustambotirov88@gmail.com>
2020-01-05 10:22:38 +0200

    render data

--- index.js
+++ index.js
@@ -16,10 +16,22 @@ app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'Home page',
+        isHome: true
+    });
 });
-app.get('/about', (req, res) => {
-    res.render('about')
+app.get('/add', (req, res) => {
+    res.render('add', {
+        title: 'Add course',
+        isAdd: true
+    })
+})
+app.get('/courses', (req, res) => {
+    res.render('courses', {
+        title: 'Courses page',
+        isCourses: true
+    })
 })
 
 const PORT = process.env.PORT || 3000;
