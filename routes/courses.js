1e70f38dcb225409547ea0037d945e95c5f895f0
Rustam Botirov <rustambotirov88@gmail.com>
2020-01-13 12:17:31 +0200

    render data

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
 const router = Router();
+const Course = require('../models/course');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const courses = await Course.getAll();
     res.render('courses', {
         title: 'Courses page',
-        isCourses: true
+        isCourses: true,
+        courses
     });
 })
 
