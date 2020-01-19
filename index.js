0530ef8a74adbccd322db22edaae9f26024ffeb5
Rustam Botirov <rustambotirov88@gmail.com>
2020-01-19 08:57:37 +0200

    connect mongo db

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
@@ -14,6 +16,7 @@ const hbs = exphbs.create({
     extname: 'hbs'
 });
 
+// set handlebars engine
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
@@ -31,6 +34,21 @@ app.use('/about', aboutRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
-});
+//define a mongoose connection
+(async function () {
+    const PASSWORD = `bwKkirh1AL7iegSL`;
+    const URL = `mongodb+srv://rustam:${PASSWORD}@cluster0-qymrr.mongodb.net/test?retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on PORT ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+
+    }
+})();
